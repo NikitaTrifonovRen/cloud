7991496f8cbfc297cef25127276ff8df6a64c9b9
NikitaTrifonovRen <SpecNas06@yandex.ru>
2024-04-18 20:53:18 +0500

    now

--- cloud-gateway/src/main/java/com/example/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/example/cloudgateway/CloudGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudGatewayApplication {
     public String name = "nothing";
+    public String name1 = "nothing1";
 
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
